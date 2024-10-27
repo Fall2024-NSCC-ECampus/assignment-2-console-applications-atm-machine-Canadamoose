c48d7ca830e6f3583a4696b32f558ee9f9557eb4
Canadamoose <canadamoose@live.ca>
2024-10-27 20:37:35 -0300

    first half of project done

--- main.cpp
+++ main.cpp
@@ -24,25 +24,50 @@ int main()
 
 void printIntroMenu()
 {
-
+    cout << "l -> Login" << endl;
+    cout << "c -> Create New Account" << endl;
+    cout << "q -> Quit" << endl;
+    cout << "> ";
 }
 
 void printMainMenu()
 {
-    // WRITE CODE HERE
+    cout << "d -> Deposit Money" << endl;
+    cout << "w -> Withdraw Money" << endl;
+    cout << "r -> Request Balance" << endl;
+    cout << "q -> Quit" << endl;
+    cout << "> ";
 }
 
-void start()
-{
-    // EXPLANATION OF CODE THAT GOES HERE IS BELOW
+void start() {
+    cout << "Please select an option from the menu below:" << endl;
+    printIntroMenu();
+    while (!(cin >> menuInput) && (menuInput != 'q' || menuInput != 'l' || menuInput != 'c')) {
+        cout << "Invalid entry." << endl;
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    switch (menuInput) {
+        case 'l':
+            login();
+            break;
+        case 'c':
+            createAccount();
+            break;
+        case 'q':
+            break;
+        default:
+            cout << "Invalid input." << endl;
+            start();
+    }
 }
 
 void createAccount()
 {
-    // PHASE 2
+    cout << "Create New Account" << endl;
 }
 
 void login()
 {
-    // PHASE 2
+    cout << "Login" << endl;
 }
