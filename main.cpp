4a2005bd9cfd440921c30c7d6c9f734493a5c574
Canadamoose <canadamoose@live.ca>
2024-10-27 19:29:28 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+// function prototypes
+void printIntroMenu();
+void printMainMenu();
+void start();
+void login();
+void createAccount();
+
+// global variable (use this variable to store the user’s menu selection)
+char menuInput;
+
+// the main function
+int main()
+{
+    // TO WRITE A WELCOME MESSAGE HERE
+    // call the function start
+    start();
+    return 0;
+}
+
+void printIntroMenu()
+{
+
+}
+
+void printMainMenu()
+{
+    // WRITE CODE HERE
+}
+
+void start()
+{
+    // EXPLANATION OF CODE THAT GOES HERE IS BELOW
+}
+
+void createAccount()
+{
+    // PHASE 2
+}
+
+void login()
+{
+    // PHASE 2
+}
